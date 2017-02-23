b001b4d68e254aef2e0a4e71c9fcd1b60d616b46
Robertas <robciun@gmail.com>
2017-02-23 09:19:21 +0200

    user session

--- vk-auth/client.js
+++ vk-auth/client.js
@@ -3,62 +3,88 @@ var soap = require('soap');
 var url = 'http://localhost:8001/tears?wsdl';
 
 var express = require('express')
+var exSession = require('express-session')
 var bodyparser = require('body-parser')
 var app = express()
-//var email ="EZ";
 app.use(bodyparser.urlencoded({"extended":true}));
 
 app.use("/login",express.static("html",{index:"login.html"}));
+app.use(exSession({
+	"secret":"DQW5435FWEFWERHER345HER15EGERVEH",
+	"resave":false,
+"saveUninitialized":true}));
+app.get("/", function(request, response){
+	if (request.session["sessionUser"])
+	{
+		response.send("Hello " + request.session["email"]+'<br><a href="/logout">Logout</a>')
+	}
+	else
+	{
+		response.redirect("/login");
+	}
+});
+
+app.get("/logout", function(request, response){
+	if (request.session["sessionUser"])
+	{
+		request.session.destroy(function(error){
+		if(error)
+		{
+			throw error;
+		}
+		response.redirect("/login");
+		});
+	}
+	else
+	{
+		response.redirect("/login");
+	}
+});
+
 app.post("/login", function(request, response){
 	
 	soap.createClient(url, function(error, client) {
     if (error) {
+		response.send("Serverio klaida!!!");
         throw error;
     }
 
     var data = {
-        email:      request.body["email"],//"labas@gmail.com", // Fill your name
-        password:    request.body["pw"]//"password", // Fill your last name
+        email:      request.body["email"],
+        password:    request.body["pw"]
     }
 
     client.describe().AuthenticationService.authenticationPort;
     client.save(data,function(err,res){
-            if (err) throw err;
-
-            // Response from web service
+            if (err) 
+			{
+				response.send("Serverio klaida!!!");
+				throw err;
+			}
             console.log (res);
-			//app.get('/', function (req, res) {
-			response.send(res);
-			//});
-			
+			if (res["isLogin"]==true || res["isLogin"]=="true")
+			{
+				request.session.regenerate(function(error){
+				if(error)
+				{
+					response.send("Serverio klaida!!!");
+					throw error;					
+				}
+				request.session["sessionUser"]=true;
+				request.session["email"]= request.body["email"];
+				response.redirect("/");
+				})
+			}
+			else
+			{
+				response.send("Nepavyko prisijungti");
+			}			
     });
 });
 	
 });
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('VK-auth app listening on port 3000!')
 })
 
 
-/*soap.createClient(url, function(error, client) {
-    if (error) {
-        throw error;
-    }
-
-    var data = {
-        email:      "labas@gmail.com", // Fill your name
-        password:  "15264879845dawd1514", // Fill your last name
-    }
-
-    client.describe().AuthenticationService.authenticationPort;
-    client.save(data,function(err,res){
-            if (err) throw err;
-
-            // Response from web service
-            console.log (res);
-			app.get('/', function (req, res) {
-			res.send(data)
-			});
-			
-    });
-});*/
--- vk-auth/service.js
+++ vk-auth/service.js
@@ -14,39 +14,6 @@ function validUser(args)
 	return -1;
 }
 
-/*function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function checkCookie() {
-	//var userCookie = document.getElementById("email");
-	var userCookie = getCookie(args["email"]);
-	if (userCookie != "" && userCookie != null) 
-	{
-	   setCookie(args["email"], userCookie, 30);
-	   alert("welcome" + userCookie);
-	}
-}*/
-
 var service = {
     AuthenticationService : {
         authenticationPort : {
@@ -56,13 +23,8 @@ var service = {
 				{
 					authenticationCorrect = true;
 				}
-				//var showUser = false;
                 return {
-					
-					"Hello": args["email"]
-					//checkCookie();
-					//isLogin: true
-                    //formatedText: args.email + " " + args.password 
+					isLogin: authenticationCorrect
                 }
             }
         }
