18a24cfe83da759bfb949a466165f9d92c0596e2
Robertas <robciun@gmail.com>
2017-05-15 20:49:02 +0300

    auth, lectures

--- nodeNew/dbConnections/lecture.js
+++ nodeNew/dbConnections/lecture.js
@@ -1,15 +1,17 @@
 var mongoose = require('mongoose');
 var dbLectures = new Schema({
-  lecturerName: String,
-  theme: String,
-  level: String,
   programmingLanguage: String,
-  tasks: [
-    {
-      task: String,
-      answer: String
-    }
-  ]
+  lecturerName: String,
+  level: String,
+  lectures:[{
+    name: String,
+    tasks: [
+      {
+        task: String,
+        answer: String
+      }
+    ]
+  }]
 });
 
 module.exports = mongoose.model('Lecture', dbLectures);
--- nodeNew/vk-lectures/lectures.js
+++ nodeNew/vk-lectures/lectures.js
@@ -1,40 +1,98 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 var app = express();
+var mongoose = require('mongoose');
+var lecture = require('./dbConnections/lecture.js');
 
 app.get('/', function(req, res) {
 	res.send('respond with a resource');
 });
 
-app.get('/userlist', function(req, res) {
-	var db = req.db;
-	db.collection('userlist').find().toArray(function (err, items) {
-		res.json(items);
+app.get('/lectureList', function(req, res) {
+	lecture.find({}, function(err, result) {
+		res.send(result);
 	});
 });
 
-app.post('/adduser', function(req, res) {
-	var db = req.db;
+app.post('/addLecture', function(req, res) {
+
+	var lect = {
+		programmingLanguage: req.body.programmingLanguage,
+		lecturerName: req.body.lecturerName,
+		level: req.body.level,
+		lectures: []
+	};
+
+	for (var i = 0; i < req.body.lectures.length; i++) {
+		lect.lectures.push({
+			name: req.body.lectures[i].name,
+			tasks: []
+		});
+		for (var j = 0; j < req.body.lectures[i].tasks.length; j++) {
+			var tsk = {
+				task: req.body.lectures[i].tasks[j].task,
+				answer: req.body.lectures[i].tasks[j].answer,
+			};
+			lect.lectures[lect.lectures.length-1].tasks.push(tsk);
+		};
+	};
+
+	var newLecture = new Lecture(lect);
+	newLecture.save(function(err, res) {
+		if (err) {
+			return console.log(err);
+		}
+		console.log('Lecture created');
+
+		res.send(result);
+	/*var db = req.db;
 	db.collection('userlist').insert(req.body, function(err, result){
 		res.send(
-		(err === null) ? { msg: '' } : { msg: err });
+		(err === null) ? { msg: '' } : { msg: err });*/
 	});
 });
 
-app.put('/updateuser/:id', function(req, res) {
-	var db = req.db
+app.post('/addTask/:id', function(req, res) {
+	Lecture.findById(req.params.id, function(err, newLecture) {
+		newLecture.tasks.push({
+			task: req.body.task,
+			answer: req.body.answer
+		});
+		newLecture.save();
+		res.send(newLecture);
+	})
+});
+
+app.put('/updateLecture/:id', function(req, res) {
+	Lecture.findById(req.params.id, {new: true}, function(err, newLecture) {
+		for (var field in Lecture.schema.paths) {
+			if((field !== '_id') && (field !== '_v')) {
+				newLecture[field] = req.body[field];
+			}
+		}
+	}
+	newLecture.save();
+ }
+ res.send(newLecture);
+ })
+});
+
+	/*var db = req.db
 	var userToUpdate = req.params.id;
 	var doc = { $set: req.body};
 	db.collection('userlist').updateById(userToUpdate, doc, function(err, result) {
 		res.send((result === 1) ? { msg: '' } : { msg:'error: ' + err });
 	});
-});
+});*/
 
-app.delete('/deleteuser/:id', function(req, res) {
-	var db = req.db;
+app.delete('/deleteLecture/:id', function(req, res) {
+	Lecture.findByIdAndRemove(req.params.id, function(err) {
+		res.send('Successful delete');
+	/*var db = req.db;
 	var userToDelete = req.params.id;
 	db.collection('userlist').removeById(userToDelete, function(err, result) {
-		res.send((result === 1) ? { msg: '' } : { msg:'error: ' + err});
+		res.send((result === 1) ? { msg: '' } : { msg:'error: ' + err});*/
 	});
 });
 
